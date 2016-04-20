6ee601adb7ea086c122d7972dac00e7b3386997f
Iam Programmer <iamprogrammerlk@gmail.com>
2016-04-20 07:46:56 +0530

    Init

---
+++ IamSource/IamGitHubComposerTester/IamGitHubComposerTester.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace IamProgrammerlk\IamGitHubComposerTester;
+
+class IamGitHubComposerTester
+{
+    public function sayHallo()
+    {
+        return '<br> I am from "IamProgrammerlk\IamGitHubComposerTester\IamGitHubComposerTester';
+    }
+}
--- IamTester/index.php
+++ IamTester/index.php
@@ -1,13 +1,3 @@
 <?php
 
  
-// define root directory
-define ('IamRoot', dirname(__DIR__), true);
-
-
-// library auto load
-require_once IamRoot . '/vendor/autoload.php';
-
-use IamProgrammerlk\IamGitHubComposerTester\HalloThere;
-
-echo HalloThere::sayHallo();
